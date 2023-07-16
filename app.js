470750f4f8001fbb0516ed25e173ea47d09806bd
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-07-16 10:04:52 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Fazendo um teste")
 console.log("Rodando o sistema local e remotamente")
+console.log("Enviando para o github")
