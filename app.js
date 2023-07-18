ca29845d0896c3e17df8cbba4ad15d1a2f13822a
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-07-18 16:48:58 -0300

    comentarios dos comandos

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ console.log("Rodando o sistema local e remotamente")
 Utilize o comando git status, ele serve para listar todos os arquivos que foram modificados.
 
 2 - Para adicionar as mudanças ao seu repositório local:
-Para adicionar todas as modificações realizadas de uma só vez, é necessário usar git add . (git add e um ponto) e, para adicionar as mudanças em algum arquivo específico, usa-se git add nome-do-arquivo-alterado.
+Para adicionar todas as modificações realizadas de uma só vez, é necessário usar git add . (git add e um ponto) e,
+ para adicionar as mudanças em algum arquivo específico, usa-se git add nome-do-arquivo-alterado.
 
 3 - Para salvar as alterações:
 Utilize o comando git commit, ele é usado quando queremos capturar e salvar o estado atual do repositório.
 
 4 - Para enviar as modificações ao repositório remoto:
 Utilize o comando git push, ele é utilizado para envio das alterações gravadas no diretório local para o repositório remoto.
+
+
+1: git add nome-do-arquivo
+2: git commit nome-do-arquivo -m "blablabla"
+3: git push
+
 */
